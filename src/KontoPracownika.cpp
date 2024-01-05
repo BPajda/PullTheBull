fa5388875b5dc1b18414786419082a91a7506cc4
BPajda <pajdabartosz02@gmail.com>
2024-01-05 21:55:04 +0100

    poprawa kontoPracownika

---
+++ src/KontoPracownika.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include "KontoPracownika.h"
+
+using namespace std;
+
+void KontoPracownika::zarzadzajCennikiem()
+{
+}
+
+void KontoPracownika::zarzadzajKontami()
+{
+}
+
+void KontoPracownika::zarzadzajHarmoZajec()
+{
+}
+
+void KontoPracownika::wpiszDaneLogowania(string login, string haslo)
+{
+}
+
+KontoPracownika::KontoPracownika()
+{
+}
+
+KontoPracownika::~KontoPracownika()
+{
+}
