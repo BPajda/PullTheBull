f9c7fd41eadbc70575573572ba913d4440b9d956
BPajda <pajdabartosz02@gmail.com>
2024-01-05 20:27:03 +0100

    rozpisanie harmonogramu

---
+++ src/HarmonogramZajec.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include "HarmonogramZajec.h"
+
+using namespace std;
+
+void HarmonogramZajec::dodajZajecia(string nazwa, int dataZajec)
+{
+}
+
+void HarmonogramZajec::usunZajecia(string nazwa, int dataZajec)
+{
+}
+
+void HarmonogramZajec::zmienZajecia(string nazwa, int dataZajec, int nowaData)
+{
+}
+
+void HarmonogramZajec::przegladajHarmonogram()
+{
+}
+
+HarmonogramZajec::HarmonogramZajec()
+{
+}
+
+HarmonogramZajec::~HarmonogramZajec()
+{
+}
