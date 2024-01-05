30439cb8acdfc5346e03de11e710ffff00660507
LukaSparrow <wrobel.lukasz02@gmail.com>
2024-01-05 12:38:43 +0100

    Switch dla uzytkownikow bez upwarnien

--- src/main.cpp
+++ src/main.cpp
@@ -12,12 +12,11 @@ int main() {
 		char wybor;
 
 		system("cls");
-		cout << "=======================" << endl;
-		cout << "Wybierz opcje:" << endl;
-		cout << "0) Logowanie" << endl;
-		cout << "1) Rejestracja" << endl;
-		cout << "2) Wyjscie" << endl;
-		cout << "=======================" << endl;
+		cout << "========= MENU =========" << endl;
+		cout << " 1) Logowanie" << endl;
+		cout << " 2) Rejestracja" << endl;
+		cout << "ESC) Wyjscie" << endl;
+		cout << "========================" << endl;
 		wybor = _getch();
 
 		string login = "";
@@ -32,9 +31,10 @@ int main() {
 		string telefon;
 		string email;
 		string wiersz;
+		char uprawnienia;
 
 		switch (wybor) {
-			case '0':
+			case '1':
 				plik.open("src/Konta.txt", ios::in);
 				system("cls");
 				while (!zalogowano) {
@@ -60,7 +60,11 @@ int main() {
 					cout << endl;
 					//
 					while (plik >> daneLogin >> daneHaslo && !zalogowano) {
-						getline(plik, wiersz);												// pominiecie reszty danych w bie¿¹cej linijce
+						//getline(plik, wiersz);											// pominiecie reszty danych w bie¿¹cej linijce
+						for (int i = 0; i < 5; i++) {
+							plik >> wiersz;
+						}
+						plik >> uprawnienia;
 						if (login == daneLogin && haslo == daneHaslo) {
 							zalogowano = true;
 						}
@@ -78,15 +82,48 @@ int main() {
 					}	
 				}
 				while (zalogowano) {
-					cout << "Zalogowano, wpisz \"x\" aby wrocic do menu" << endl;
-					if (_getch() == 'x') {
-						goto menu;
+					system("cls");
+					switch (uprawnienia) {
+					case '0':
+						cout << "==== Zalogowano jako " << login << " ====" << endl;
+						cout << " 1) Wyswietl Karnet" << endl;
+						cout << " 2) Przegladaj Cennik" << endl;
+						cout << " 3) Moje Zajecia" << endl;
+						cout << " 4) Przegladaj Zajecia" << endl;
+						cout << " 5) Rezerwacja Zajec" << endl;
+						cout << " 6) Usun Konto" << endl;
+						cout << "ESC) Powrot" << endl;
+						wybor = _getch();
+						switch (wybor) {
+						case '1':
+							break;
+						case '2':
+							break;
+						case '3':
+							break;
+						case '4':
+							break;
+						case '5':
+							break;
+						case '6':
+							break;
+						case 27:
+							goto menu;
+						default:
+							break;
+						}
+						break;
+					case '1':
+						break;
+					default:
+						wybor = '0';
+						break;
 					}
 				}
 				cout << "Pomyslnie zalogowano!" << endl;
 				plik.close();
 				break;
-			case '1':
+			case '2':
 				cout << "======== Tworzenie konta ========" << endl;
 				cout << "Podaj imie: ";
 				cin >> imie;
@@ -131,7 +168,7 @@ int main() {
 				plik.close();
 				
 				break;
-			case '2':
+			case 27:
 				dziala = false;
 				break;
 			default:
